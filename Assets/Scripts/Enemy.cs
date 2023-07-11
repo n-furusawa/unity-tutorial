a01f426dccdeef64c21b3f242c9a5fb7562dc715
n-furusawa <85210413+n-furusawa@users.noreply.github.com>
2023-07-11 15:31:20 +0900

    ゲーム編完了

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    // ヒットポイント
+    public int hp = 1;
+
+    // スコアポイント
+    public int point = 100;
+
+    // Spaceshipコンポーネント
+    Spaceship spaceship;
+
+    IEnumerator Start ()
+    {
+
+        // Spaceshipコンポーネントを取得
+        spaceship = GetComponent<Spaceship> ();
+
+        // ローカル座標のY軸のマイナス方向に移動する
+        spaceship.Move (transform.up * -1);
+
+        // canShotがfalseの場合、ここでコルーチンを終了させる
+        if (spaceship.canShot == false) {
+            yield break;
+        }
+
+        while (true) {
+
+            // 子要素を全て取得する
+            for (int i = 0; i < transform.childCount; i++) {
+
+                Transform shotPosition = transform.GetChild(i);
+
+                // ShotPositionの位置/角度で弾を撃つ
+                spaceship.Shot (shotPosition);
+            }
+
+            // shotDelay秒待つ
+            yield return new WaitForSeconds (spaceship.shotDelay);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D c)
+    {
+        // レイヤー名を取得
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        // レイヤー名がBullet (Player)以外の時は何も行わない
+        if( layerName != "Bullet(Player)") return;
+
+        // PlayerBulletのTransfomrmを取得
+        Transform playerBulletTransform = c.transform.parent;
+
+        // Bulletコンポーネントを取得
+        Bullet bullet = playerBulletTransform.GetComponent<Bullet>();
+
+        //ヒットポイントを減らす
+        hp = hp - bullet.power;
+
+        // 弾の削除
+        Destroy(c.gameObject);
+
+        // ヒットポイントが0以下であれば
+        if (hp < 0) {
+
+            // スコアコンポーネントを取得してポイントを追加
+            FindObjectOfType<Score>().AddPoint(point);
+            
+            // 爆発
+            spaceship.Explosion();
+
+            // エネミーの削除
+            Destroy(gameObject);
+        } else {
+            spaceship.GetAnimator().SetTrigger("Damage");
+        }
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    void OnAnimationFinish ()
+    {
+        Destroy (gameObject);
+    }
+}
